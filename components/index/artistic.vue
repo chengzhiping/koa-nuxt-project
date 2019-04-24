3c695a2fa95ce3a65fba04376858499baf446b8c
zhangjunling <zhangjunling@boyi365.cn>
2019-04-24 22:18:31 +0800

    完善content和footer

---
+++ components/index/artistic.vue
@@ -0,0 +1,88 @@
+<template>
+  <section class="m-istyle">
+    <dl @mouseover="over">
+      <dt>有格调</dt>
+      <dd
+        :class="{active:kind==='all'}"
+        kind="all"
+        keyword="景点"
+      >
+        全部
+      </dd>
+      <dd
+        :class="{active:kind==='part'}"
+        kind="part"
+        keyword="美食"
+      >
+        约会聚餐
+      </dd>
+      <dd
+        :class="{active:kind==='spa'}"
+        kind="spa"
+        keyword="丽人"
+      >
+        丽人SPA
+      </dd>
+      <dd
+        :class="{active:kind==='movie'}"
+        kind="movie"
+        keyword="电影"
+      >
+        电影演出
+      </dd>
+      <dd
+        :class="{active:kind==='travel'}"
+        kind="travel"
+        keyword="旅游"
+      >
+        品质出游
+      </dd>
+    </dl>
+    <ul class="ibody">
+      <li v-for="(item, index) in 6" :key="index">
+        <el-card shadow="never" :body-style="{ padding: '0px' }">
+          <img src="http://p0.meituan.net/msmerchant/f0f72ccd78773cc5a0156b1c00d588c0961404.jpg@460w_260h_1e_1c" class="image">
+          <ul class="cbody">
+            <li class="title">
+              大动 王附件点
+            </li>
+            <li class="pos">
+              春季堂处于黄玉套餐
+            </li>
+            <li class="price">
+              ￥<em>{{ 17 }}</em><span>/起</span>
+            </li>
+          </ul>
+        </el-card>
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      kind: 'all',
+      list: {
+        all: [],
+        part: [],
+        spa: [],
+        movie: [],
+        travel: []
+      }
+    }
+  },
+  computed: {
+    cur() {
+      return this.list[this.kind]
+    }
+  },
+  methods: {
+    over() {
+    }
+  }
+}
+</script>
+<style lang="scss">
+ @import "@/assets/css/index/artistic.scss";
+</style>
