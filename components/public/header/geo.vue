e0be3f12910a99c1f4767fce26f0df628431608d
zhangjunling <zhangjunling@boyi365.cn>
2019-04-24 08:16:26 +0800

    首次完成header部分的编写

---
+++ components/public/header/geo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location">北京市</i>
+    <nuxt-link
+      class="changeCity"
+      to="/changeCity"
+    >
+      切换城市
+    </nuxt-link>
+    [南昌 九江 深圳]
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/public/header/nav.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="m-nav">
+    <ul class="nav">
+      <li class="list">
+        <nuxt-link to="/my">
+          我的美团
+        </nuxt-link>
+        <dl>
+          <dd>
+            <nuxt-link to="/order">
+              我的订单
+            </nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/order">
+              我的收藏
+            </nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/order">
+              抵用券
+            </nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/order">
+              账户设置
+            </nuxt-link>
+          </dd>
+        </dl>
+      </li>
+      <li class="list">
+        <nuxt-link to="/order">
+          手机app
+        </nuxt-link>
+      </li>
+      <li class="list bd">
+        <nuxt-link to="/center">
+          商家中心
+        </nuxt-link>
+        <dl>
+          <dd>
+            <nuxt-link to="/order">
+              登录商家中心
+            </nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/order">
+              我想合作
+            </nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/order">
+              免费手机开店
+            </nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/order">
+              商家申请开票
+            </nuxt-link>
+          </dd>
+        </dl>
+      </li>
+      <li class="list site">
+        <nuxt-link to="/site">
+          网站导航
+        </nuxt-link>
+        <div class="subContainer">
+          <dl class="hotel">
+            <dt>酒店旅游</dt>
+            <dd>国际击破</dd>
+            <dd>国际击破</dd>
+            <dd>国际击破</dd>
+            <dd>国际击破</dd>
+            <dd>国际击破</dd>
+            <dd>国际击破</dd>
+          </dl>
+          <dl class="food">
+            <dt>美食</dt>
+            <dd>烧烤</dd>
+            <dd>黄焖鸡</dd>
+            <dd>特色小吃</dd>
+            <dd>烤鱼</dd>
+          </dl>
+          <dl class="movie">
+            <dt>看电影</dt>
+            <dd>热映电影</dd>
+            <dd>热映电影</dd>
+            <dd>热映电影</dd>
+            <dd>热映电影</dd>
+            <dd>热映电影</dd>
+            <dd>热映电影</dd>
+            <dd>热映电影</dd>
+          </dl>
+          <dl class="app">
+            <dt>手机应用</dt>
+            <dd>
+              <a href="#">
+                <img
+                  src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/appicons/meituan.png"
+                  title="美团app"
+                  alt="美团app">
+              </a>
+            </dd>
+            <dd>
+              <a href="#">
+                <img
+                  src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/appicons/meituan.png"
+                  title="美团app"
+                  alt="美团app">
+              </a>
+            </dd>
+            <dd>
+              <a href="#">
+                <img
+                  src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/appicons/meituan.png"
+                  title="美团app"
+                  alt="美团app">
+              </a>
+            </dd>
+            <dd>
+              <a href="#">
+                <img
+                  src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/appicons/meituan.png"
+                  title="美团app"
+                  alt="美团app">
+              </a>
+            </dd>
+            <dd>
+              <a href="#">
+                <img
+                  src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/appicons/meituan.png"
+                  title="美团app"
+                  alt="美团app">
+              </a>
+            </dd>
+          </dl>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
